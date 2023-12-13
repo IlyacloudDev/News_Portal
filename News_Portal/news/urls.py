0bb3ec0b1d9b5f6dccc879ac22c4f0caab3f9912
Ilya Stritskiy <msv_ilya@mail.ru>
2023-12-13 16:48:40 +0300

    urls.py: added new url for searching news

--- News_Portal/news/urls.py
+++ News_Portal/news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostSearch
 
 
 urlpatterns = [
@@ -9,8 +9,9 @@ urlpatterns = [
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
-   path('', PostList.as_view()),
+   path('', PostList.as_view(), name='posts_list'),
    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
    # int — указывает на то, что принимаются только целочисленные значения
-   path('<int:pk>', PostDetail.as_view()),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('search/', PostSearch.as_view(), name='post_search')
 ]

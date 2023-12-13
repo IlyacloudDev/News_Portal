1ff6504a3c2558e2b5a335720e7dd5cf8ee92de8
Ilya Stritskiy <msv_ilya@mail.ru>
2023-12-13 16:51:46 +0300

    views.py: added new view to filtering on the way /search

--- News_Portal/news/views.py
+++ News_Portal/news/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 
 
 # Create your views here.
 class PostList(ListView):
     model = Post
     ordering = '-time_in'
-    template_name = 'news.html'
+    template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 10
 
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'newsdetail.html'
+    template_name = 'postdetail.html'
     context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['filterset'] = self.filterset
+        return context

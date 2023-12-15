3383e76b139db690750e5007c53b3d3d1861c10d
Ilya Stritskiy <msv_ilya@mail.ru>
2023-12-15 22:53:16 +0300

    urls.py: added new suffixes to go over on searching; creating; updating; deleting pages

--- News_Portal/news/urls.py
+++ News_Portal/news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import PostList, PostDetail, PostSearch
+from .views import (PostList, PostDetail,
+                    PostSearch, NewsCreate,
+                    NewsUpdate, NewsDelete,
+                    ArticleCreate, ArticleUpdate,
+                    ArticleDelete,)
 
 
 urlpatterns = [
@@ -13,5 +17,13 @@ urlpatterns = [
    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
    # int — указывает на то, что принимаются только целочисленные значения
    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
-   path('search/', PostSearch.as_view(), name='post_search')
+   path('search/', PostSearch.as_view(), name='post_search'),
+
+   path('news/create/', NewsCreate.as_view(), name='news_creating'),
+   path('news/<int:pk>/edit/', NewsUpdate.as_view(), name='news_updating'),
+   path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_deleting'),
+
+   path('articles/create/', ArticleCreate.as_view(), name='article_creating'),
+   path('articles/<int:pk>/edit/', ArticleUpdate.as_view(), name='article_updating'),
+   path('articles/<int:pk>/delete/', ArticleDelete.as_view(), name='article_deleting'),
 ]

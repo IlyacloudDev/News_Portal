7bef54847a8631a8c52148f6bcd02458c969a18d
Ilya Stritskiy <msv_ilya@mail.ru>
2023-12-15 22:48:12 +0300

    views.py: added new views for creating; updating; deleting

--- News_Portal/news/views.py
+++ News_Portal/news/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+
 from .models import Post
 from .filters import PostFilter
+from .forms import NewsForm, ArticleForm
 
 
 # Create your views here.
@@ -10,7 +13,7 @@ class PostList(ListView):
     ordering = '-time_in'
     template_name = 'posts.html'
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 1
 
 
 class PostDetail(DetailView):
@@ -34,3 +37,55 @@ class PostSearch(ListView):
         context = super().get_context_data()
         context['filterset'] = self.filterset
         return context
+
+
+class NewsCreate(CreateView):
+    form_class = NewsForm
+    model = Post
+    template_name = 'news_update.html'
+
+    def form_valid(self, form):
+        form.instance.type_of_post = Post.news
+        return super().form_valid(form)
+
+
+class NewsUpdate(UpdateView):
+    form_class = NewsForm
+    model = Post
+    template_name = 'news_update.html'
+
+    def form_valid(self, form):
+        form.instance.type_of_post = Post.news
+        return super().form_valid(form)
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+
+class ArticleCreate(CreateView):
+    form_class = ArticleForm
+    model = Post
+    template_name = 'article_update.html'
+
+    def form_valid(self, form):
+        form.instance.type_of_post = Post.article
+        return super().form_valid(form)
+
+
+class ArticleUpdate(UpdateView):
+    form_class = ArticleForm
+    model = Post
+    template_name = 'article_update.html'
+
+    def form_valid(self, form):
+        form.instance.type_of_post = Post.article
+        return super().form_valid(form)
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('posts_list')

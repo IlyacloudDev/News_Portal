9ebddb0c020c04c6ff5091c0979f2a12d477df42
Ilya Stritskiy <msv_ilya@mail.ru>
2024-02-24 16:42:53 +0300

    was added task according to the schedule

--- News_Portal/subscriptions/tasks.py
+++ News_Portal/subscriptions/tasks.py
@@ -1,24 +1,26 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
-from django.db.models.signals import m2m_changed
-from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+import datetime
 
 from celery import shared_task
 
 from News_Portal import settings
-
-from news.models import PostCategory, Post
+from news.models import PostCategory, Post, Category
 
 
 @shared_task
 def post_created(pk, **kwargs):
 
     post = Post.objects.get(pk=pk)
-    emails = User.objects.filter(
+    emails = set(User.objects.filter(
         subscriptions__category__in=post.category.all()
-    ).values_list('email', flat=True)
+    ).values_list('email', flat=True))
 
-    subject = f'Новый пост в категории "{post.heading}"'
+    subject = f'Новый пост в категории(-ях) ...'
 
     text_content = (
         f'Пост: {post.heading}\n'
@@ -33,3 +35,31 @@ def post_created(pk, **kwargs):
         msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [email])
         msg.attach_alternative(html_content, "text/html")
         msg.send()
+
+
+@shared_task
+def weekly_email():
+
+    today = timezone.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(time_in__gte=last_week)
+    categories = set(posts.values_list('category__name_of_category', flat=True))
+    subscribers = set(Category.objects.filter(name_of_category__in=categories).values_list('subscriptions__user__email', flat=True))
+
+    html_content = render_to_string(
+        'daily_posts.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()

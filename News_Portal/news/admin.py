65ee031e6bcdd4171c2d98924b2ec22ef0b050ae
Ilya Stritskiy <msv_ilya@mail.ru>
2024-04-22 22:39:22 +0300

    was added configuration for translation

--- News_Portal/news/admin.py
+++ News_Portal/news/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from .models import Post, Category, PostCategory, Author
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin, TranslationAdmin):
+    model = Post
     list_display = ('id', 'heading', 'type_of_post', 'posts_categories')
     list_filter = ('category',)
     search_fields = ('heading', 'author__authorUser__username')
@@ -12,8 +14,12 @@ class PostAdmin(admin.ModelAdmin):
         return ', '.join(cat.get_name_of_category_display() for cat in p.category.all())
 
 
+class CategoryAdmin(TranslationAdmin):
+    model = Category
+
+
 # Register your models here.
 admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Author)

a8a80407975955e8d1bde4354f3c6d61af54b517
Ilya Stritskiy <msv_ilya@mail.ru>
2024-04-24 20:59:37 +0300

    was fixed some mistakes

--- News_Portal/news/translation.py
+++ News_Portal/news/translation.py
@@ -2,12 +2,6 @@ from .models import Category, Post
 from modeltranslation.translator import register, TranslationOptions # импортируем декоратор для перевода и класс
 
 
-# настроек, от которого будем наследоваться
-@register(Category)
-class CategoryTranslationOptions(TranslationOptions):
-    fields = ('name_of_category', ) # указываем, какие именно поля надо переводить в виде кортежа
-
-
 @register(Post)
 class PostTranslationOptions(TranslationOptions):
-    fields = ('type_of_post', 'heading', 'text_of_post', 'category') # указываем, какие именно поля надо переводить в виде кортежа
+    fields = ('heading', 'text_of_post') # указываем, какие именно поля надо переводить в виде кортежа

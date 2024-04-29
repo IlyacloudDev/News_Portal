bdbf5ca9ba51014806686b778a9275dc617cc8f7
Ilya Stritskiy <msv_ilya@mail.ru>
2024-04-30 00:03:50 +0300

    was added translation of fields for search

--- News_Portal/news/filters.py
+++ News_Portal/news/filters.py
@@ -1,16 +1,18 @@
 from django_filters import (FilterSet, DateTimeFilter, CharFilter)
 from django import forms
+from django.utils.translation import gettext_lazy as _
+
 from .models import Post
 
 
 class PostFilter(FilterSet):
     heading = CharFilter(
-        label='Heading',
+        label=_('Heading'),
         lookup_expr='iregex'
     )
 
     time_after = DateTimeFilter(
-        label='Post was created earlier than:',
+        label=_('Post was created earlier than:'),
         field_name='time_in',
         lookup_expr='gt',
         widget=forms.DateTimeInput(

0430973d5e07cc02eb29c2fca631357931c23be2
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-08 00:05:56 +0300

    was added new url for using OpenAPI specfication

--- News_Portal/News_Portal/urls.py
+++ News_Portal/News_Portal/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('posts/', include('news.urls')),
+    path('', include('news.urls')),
     path('subscriptions/', include('subscriptions.urls')),
-    path('i18n/', include('django.conf.urls.i18n'))
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema',}
+    ), name='swagger-ui'),
 ]
